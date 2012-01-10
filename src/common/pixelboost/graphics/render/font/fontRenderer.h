25f3995848ba753497e122e35f5cec6d3d6198bf
Jamie Hales <jamie@pixelballoon.com>
2012-01-10 11:51:24 +0000

    Change default font scale to 1 unit

--- src/common/pixelboost/graphics/render/font/fontRenderer.h
+++ src/common/pixelboost/graphics/render/font/fontRenderer.h
@@ -33,7 +33,7 @@ namespace pixelboost
         virtual void Update(float time);
         virtual void Render(RenderLayer* layer);
         
-        bool AttachToRenderer(RenderLayer* layer, const std::string& fontName, const std::string& string, Vec2 position, FontAlign alignment = kFontAlignCenter, float scale = 32.f, float rotation = 0.f, Vec4 color = Vec4(1.f, 1.f, 1.f, 1.f));
+        bool AttachToRenderer(RenderLayer* layer, const std::string& fontName, const std::string& string, Vec2 position, FontAlign alignment = kFontAlignCenter, float scale = 1.f, float rotation = 0.f, Vec4 color = Vec4(1.f, 1.f, 1.f, 1.f));
         
         void SplitString(const std::string& string, char seperator, std::vector<std::string>& output);
         
