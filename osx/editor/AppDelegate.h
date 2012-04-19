637751df5bcbac344348f6acaa895030c33633ec
Jamie Hales <jamie@pixelballoon.com>
2012-04-19 20:55:00 +0100

    Add Mac OS X project

---
+++ osx/editor/AppDelegate.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet WebView *webView;
+
+@end
---
+++ osx/editor/AppDelegate.m
@@ -0,0 +1,21 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize webView = _webView;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    [self.webView setMaintainsBackForwardList:false];
+    [self.webView setShouldCloseWithWindow:true];
+    WebPreferences* preferences = [self.webView preferences];
+    [preferences setUsesPageCache:false];
+    [preferences setCacheModel:WebCacheModelDocumentBrowser];
+    [preferences setPrivateBrowsingEnabled:true];
+    [self.webView setPreferences:preferences];
+    
+    [self.webView setMainFrameURL:@"http://localhost:9090"];
+}
+
+@end
