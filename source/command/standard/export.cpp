385258ca8d8f16d4d939ea3c37b9c2d768196d73
Jamie Hales <jamie@pixelballoon.com>
2012-04-22 15:52:14 +0100

    Remove template generator

--- source/command/standard/export.cpp
+++ source/command/standard/export.cpp
@@ -2,7 +2,6 @@
 
 #include "command/standard/file.h"
 #include "project/project.h"
-#include "project/template.h"
 #include "core.h"
 
 using namespace pixeleditor;
@@ -24,14 +23,10 @@ std::string ExportCommand::GetName()
 
 bool ExportCommand::Do()
 {
-    TemplateGenerator generator(Core::Instance()->GetProject()->GetSchema());
-    
     char cmd[2048];
     std::string outputDir = Core::Instance()->GetProject()->GetConfig().exportDir + "templates/";
     sprintf(cmd, "mkdir -p %s", outputDir.c_str());
     system(cmd);
-    
-    generator.GenerateTemplates(outputDir);
     
     return Core::Instance()->GetProject()->Export();
 }
