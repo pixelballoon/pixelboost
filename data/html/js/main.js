4f839e10ed3ae25c25132a5fd19268794751133a
Jamie Hales <jamie@pixelballoon.com>
2012-05-06 13:49:57 +0100

    Clean up dependencies

---
+++ data/html/js/main.js
@@ -0,0 +1,4 @@
+require(["order!jquery-1.7.2.min", "order!jquery.noty",
+		 "order!jquery.mousewheel", "order!underscore-min",
+		 "order!bootstrap.min", "order!bootstrap-dropdown",
+		 "order!kinetic.min", "order!pixeleditor"]);
