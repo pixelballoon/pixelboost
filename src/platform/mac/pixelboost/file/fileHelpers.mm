851901da54705465cb199740f5fcd8a4b9767e9d
Jamie Hales <jamie@pixelballoon.com>
2012-05-29 02:27:56 +0100

    Add mac implementation of file helper functions

--- src/platform/mac/pixelboost/file/fileHelpers.mm
+++ src/platform/mac/pixelboost/file/fileHelpers.mm
@@ -15,6 +15,42 @@ std::string GetRootPath()
     [pool release];
     return rootPath;
 }
+    
+std::string GetUserPath()
+{
+    return "/";
+}
+    
+bool pb::FileHelpers::CreateDirectory(const std::string& directory)
+{
+    NSString* dirToCreate = [NSString stringWithUTF8String:directory.c_str()];
+    NSError* error = nil;
+    BOOL isDir=YES;
+    
+    if([[NSFileManager defaultManager] fileExistsAtPath:dirToCreate isDirectory:&isDir] || !isDir)
+        return false;
+    
+    if(![[NSFileManager defaultManager] createDirectoryAtPath:dirToCreate withIntermediateDirectories:YES attributes:nil error:&error])
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+int pb::FileHelpers::GetTimestamp(const std::string& filename)
+{
+    NSDictionary* attrs = [[NSFileManager defaultManager] attributesOfItemAtPath:[NSString stringWithUTF8String:filename.c_str()] error:nil];
+    
+    if (attrs != nil)
+    {
+        NSDate *date = (NSDate*)[attrs objectForKey: NSFileModificationDate];
+        return (int)[date timeIntervalSince1970];
+    } 
+    else {
+        return 0;
+    }
+}
         
 }
     
