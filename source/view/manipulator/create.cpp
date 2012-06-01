27690aaefe88d32d9abfb36a244c44b96158d825
Jamie Hales <jamie@pixelballoon.com>
2012-06-02 00:02:23 +0100

    Fix crash when trying to create entity while no record is loaded

--- source/view/manipulator/create.cpp
+++ source/view/manipulator/create.cpp
@@ -43,22 +43,25 @@ bool CreateManipulator::OnMouseDown(pb::MouseButton button, pb::ModifierKeys mod
 {
     if (button == pb::kMouseButtonLeft)
     {
-        glm::vec2 start = View::Instance()->GetLevelCamera()->ConvertScreenToWorld(position);
-        
-        char commandArgs[1024];
-        sprintf(commandArgs, "-r %d -t %s -p %f,%f,%f", View::Instance()->GetRecord()->GetUid(), _EntityType.c_str(), start.x, start.y, 0.f);
-        std::string entityIdString = Core::Instance()->GetCommandManager()->Exec("createEntity", commandArgs);
-        Uid entityId = atoi(entityIdString.c_str());
-        Entity* entity = Core::Instance()->GetProject()->GetEntity(entityId);
-        
-        for (int i=0; i<_Fields.size(); i++)
+        if (View::Instance()->GetRecord())
         {
-            entity->AcquireAtom(_Fields[i])->SetStringValue(_Values[i]);
+            glm::vec2 start = View::Instance()->GetLevelCamera()->ConvertScreenToWorld(position);
+            
+            char commandArgs[1024];
+            sprintf(commandArgs, "-r %d -t %s -p %f,%f,%f", View::Instance()->GetRecord()->GetUid(), _EntityType.c_str(), start.x, start.y, 0.f);
+            std::string entityIdString = Core::Instance()->GetCommandManager()->Exec("createEntity", commandArgs);
+            Uid entityId = atoi(entityIdString.c_str());
+            Entity* entity = Core::Instance()->GetProject()->GetEntity(entityId);
+            
+            for (int i=0; i<_Fields.size(); i++)
+            {
+                entity->AcquireAtom(_Fields[i])->SetStringValue(_Values[i]);
+            }
+            
+            Core::Instance()->GetCommandManager()->Exec("select", "-u " + entityIdString);
+            
+            return true;
         }
-        
-        Core::Instance()->GetCommandManager()->Exec("select", "-u " + entityIdString);
-        
-        return true;
     } else if (button == pb::kMouseButtonRight)
     {
         View::Instance()->GetManipulatorManager()->SetActiveManipulator("select");
