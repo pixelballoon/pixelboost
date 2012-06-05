b56cce91653bb1d58efa9b531cb2d8fe57a6405a
Jamie Hales <jamie@pixelballoon.com>
2012-06-05 13:55:25 +0100

    Fix up db compilation due to new namespace

--- src/common/pixelboost/db/register.h
+++ src/common/pixelboost/db/register.h
@@ -14,13 +14,13 @@
 #define PIXELBOOST_END_REGISTER }
 
 #define PIXELBOOST_START_STRUCT(type, name) \
-    class pb::Record; \
+    class pb::db::Record; \
     void* PB_Create ## type (); \
     void PB_Register ## type (); \
     void PB_Deserialise ## type (void* data); \
     void PB_Register ## type () { \
-        pb::Database::Instance()->RegisterCreate(pb::TypeHash(name), &PB_Create ## type ); \
-        pb::Database::Instance()->RegisterDeserialise(pb::TypeHash(name), &PB_Deserialise ## type ); \
+        pb::db::Database::Instance()->RegisterCreate(pb::TypeHash(name), &PB_Create ## type ); \
+        pb::db::Database::Instance()->RegisterDeserialise(pb::TypeHash(name), &PB_Deserialise ## type ); \
     } \
     void* PB_Create ## type () { \
         type* structObject = new type(); \
@@ -29,7 +29,7 @@
     void PB_Deserialise ## type (void* data) { \
         type& object = (type&)*data; \
         (void)object; \
-        lua_State* state = pb::Database::Instance()->GetLuaState(); \
+        lua_State* state = pb::db::Database::Instance()->GetLuaState(); \
         (void)state;
 
 #define PIXELBOOST_FIELD_INT(field, name) { \
