8fb1d4e5859ee7ed92fcda41578769b250d50d9d
Jamie Hales <jamie@pixelballoon.com>
2012-06-09 22:21:09 +0100

    Add basic camera component

---
+++ src/common/pixelboost/logic/component/graphics/camera.cpp
@@ -0,0 +1,54 @@
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "pixelboost/framework/game.h"
+#include "pixelboost/graphics/camera/camera.h"
+#include "pixelboost/logic/component/graphics/camera.h"
+#include "pixelboost/logic/component/transform.h"
+#include "pixelboost/logic/message/transform.h"
+#include "pixelboost/logic/entity.h"
+//#include "pixelboost/logic/scene.h"
+
+using namespace pb;
+
+CameraComponent::CameraComponent(Entity* parent, Camera* camera)
+    : Component(parent)
+{
+    _Camera = camera;
+    
+    GetParent()->RegisterMessageHandler(TransformChangedMessage::GetStaticType(), Entity::MessageHandler(this, &CameraComponent::OnTransformChanged));
+    
+    UpdateTransform();
+}
+
+CameraComponent::~CameraComponent()
+{
+    GetParent()->UnregisterMessageHandler(TransformChangedMessage::GetStaticType(), Entity::MessageHandler(this, &CameraComponent::OnTransformChanged));
+    
+    delete _Camera;
+}
+
+Uid CameraComponent::GetType()
+{
+    return GetStaticType();
+}
+
+Uid CameraComponent::GetStaticType()
+{
+    return TypeHash("camera");
+}
+
+void CameraComponent::OnTransformChanged(Uid sender, Message& message)
+{
+    UpdateTransform();
+}
+
+void CameraComponent::UpdateTransform()
+{
+    TransformComponent* transform = GetParent()->GetComponentByType<TransformComponent>();    
+    
+    if (transform)
+    {
+        _Camera->Position = transform->GetPosition();
+        _Camera->Rotation = transform->GetRotation();
+    }
+}
---
+++ src/common/pixelboost/logic/component/graphics/camera.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+#include "glm/glm.hpp"
+
+#include "pixelboost/graphics/renderer/common/renderable.h"
+#include "pixelboost/graphics/renderer/font/fontRenderer.h"
+#include "pixelboost/logic/component.h"
+
+namespace pb
+{
+    
+    class Camera;
+    class Message;
+    class FontRenderable;
+    
+    class CameraComponent : public Component
+    {
+    public:
+        CameraComponent(Entity* parent, Camera* camera);
+        virtual ~CameraComponent();
+        
+        Uid GetType();
+        static Uid GetStaticType();
+        
+    private:
+        void OnTransformChanged(Uid sender, Message& message);
+        void UpdateTransform();
+        
+        Camera* _Camera;
+    };
+    
+}
