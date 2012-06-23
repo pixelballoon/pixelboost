f0e1be91dcc4dc0a8afafd09d27d8cdf06b79acf
Jamie Hales <jamie@pixelballoon.com>
2012-06-23 20:57:50 +0100

    Shrink window for retina displays

--- osx/editor/AppDelegate.mm
+++ osx/editor/AppDelegate.mm
@@ -15,7 +15,7 @@
     _Core = new pixeleditor::Core();
     
     _View = new pixeleditor::View();
-    _View->Initialise(glm::vec2(1200,1000));
+    _View->Initialise(glm::vec2(1200,800));
     _View->Render();
 }
 
