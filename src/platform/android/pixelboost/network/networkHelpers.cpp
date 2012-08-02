70b91a44f26054e6b301c400084fb1fda316f00a
Jamie Hales <jamie@pixelballoon.com>
2012-08-02 08:30:09 +0100

    Fix GetWifiAddress on Android

--- src/platform/android/pixelboost/network/networkHelpers.cpp
+++ src/platform/android/pixelboost/network/networkHelpers.cpp
@@ -1,4 +1,4 @@
-#ifdef PIXELBOOST_PLATFORM_IOS
+#ifdef PIXELBOOST_PLATFORM_ANDROID
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -16,37 +16,7 @@ namespace NetworkHelpers
 
 std::string GetWifiAddress()
 {
-#if TARGET_IPHONE_SIMULATOR
-    return "127.0.0.1";
-#else
-    std::string address = "WiFi not enabled"; 
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    
-    int success;
-    success = getifaddrs(&interfaces); 
-    
-    if (success == 0)
-    { 
-        // Loop through linked list of interfaces  
-        temp_addr = interfaces; 
-        while(temp_addr != NULL)
-        {
-            if(temp_addr->ifa_addr->sa_family == AF_INET)
-            {
-                // en0 = iPhone WiFi connection
-                if(strcmp(temp_addr->ifa_name, "en0") == 0)
-                {
-                    address = inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr);
-                } 
-            }
-            temp_addr = temp_addr->ifa_next; 
-        } 
-    }
-    
-    freeifaddrs(interfaces); 
-    return address;
-#endif
+    return "";
 }
 
 }
