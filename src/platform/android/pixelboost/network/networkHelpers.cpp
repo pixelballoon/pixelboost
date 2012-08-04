ee6f5abd6011ec6b5617c30b4953245f034b6a4b
Jamie Hales <jamie@pixelballoon.com>
2012-08-04 13:46:08 +0100

    Fix Android compile error

--- src/platform/android/pixelboost/network/networkHelpers.cpp
+++ src/platform/android/pixelboost/network/networkHelpers.cpp
@@ -1,11 +1,5 @@
 #ifdef PIXELBOOST_PLATFORM_ANDROID
 
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <ifaddrs.h>
-
 #include "pixelboost/network/networkHelpers.h"
 
 namespace pb
