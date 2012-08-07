6b426cf674a20a4481904f3a697d83118c9ee0bd
Jamie Hales <jamie@pixelballoon.com>
2012-08-07 13:22:42 +0100

    Add more logging

--- src/common/pixelboost/debug/log.h
+++ src/common/pixelboost/debug/log.h
@@ -6,7 +6,11 @@
 	#ifdef PIXELBOOST_PLATFORM_ANDROID
 		#include <android/log.h>
 		#define PbLogDebug(system, format, ...) __android_log_print(ANDROID_LOG_DEBUG, system, format, ## __VA_ARGS__)
+        #define PbLogError(system, format, ...) __android_log_print(ANDROID_LOG_ERROR, system, format, ## __VA_ARGS__)
+        #define PbLogWarn(system, format, ...) __android_log_print(ANDROID_LOG_WARN, system, format, ## __VA_ARGS__)
 	#else
-		#define PbLogDebug(system, format, ...) printf(format, ## __VA_ARGS__)
+        #define PbLogDebug(system, format, ...) printf(format, ## __VA_ARGS__)
+		#define PbLogError(system, format, ...) printf(format, ## __VA_ARGS__)
+        #define PbLogWarn(system, format, ...) printf(format, ## __VA_ARGS__)
 	#endif
 #endif
