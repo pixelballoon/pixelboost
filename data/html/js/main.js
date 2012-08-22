e5c259a1b0f010d666ab5d39f817a36a659e49b5
Jamie Hales <jamie@pixelballoon.com>
2012-08-22 23:06:29 +0100

    Cleanup build

--- data/html/js/main.js
+++
@@ -1,4 +0,0 @@
-require(["order!jquery-1.7.2.min", "order!jquery.noty",
-		 "order!jquery.mousewheel", "order!underscore-min",
-		 "order!bootstrap.min", "order!bootstrap-dropdown",
-		 "order!kinetic.min", "order!editor/main"]);
