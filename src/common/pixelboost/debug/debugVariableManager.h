05043915cd3a4ad2d18103be889bb395dfe2a1fc
Jamie Hales <jamie@pixelballoon.com>
2012-08-24 21:27:44 +0100

    Move debug html into subdirectory

--- src/common/pixelboost/debug/debugVariableManager.h
+++ src/common/pixelboost/debug/debugVariableManager.h
@@ -18,7 +18,7 @@ class DebugVariable;
 class DebugVariableManager : public HttpServer
 {
 public:
-    DebugVariableManager(const std::string& htmlLocation = "data/debug/html");
+    DebugVariableManager(const std::string& htmlLocation = "data/pixelboost/debug/html");
     ~DebugVariableManager();
     
     static DebugVariableManager* Instance();
