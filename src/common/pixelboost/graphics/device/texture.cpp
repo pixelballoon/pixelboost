46aa0c9cbbb051eac755b5cfc4a1100c9dc3699f
Jamie Hales <jamie@pixelballoon.com>
2012-12-02 20:54:45 +0000

    Add support for reading jpeg+png alpha files

--- src/common/pixelboost/graphics/device/texture.cpp
+++ src/common/pixelboost/graphics/device/texture.cpp
@@ -55,26 +55,79 @@ bool Texture::LoadFromBytes(const unsigned char* data, int width, int height, bo
 
 bool Texture::LoadFromFile(pb::FileLocation location, const std::string& path, bool createMips, bool hasPremultipliedAlpha)
 {
-    std::vector<unsigned char> data;
-    unsigned char* decoded;
+    bool status = true;
+    
+    if (path.substr(path.length()-4) == ".jpa")
+    {
+        pb::File* file = pb::FileSystem::Instance()->OpenFile(location, path);
+        if (!file)
+            return false;
+        
+        int rgbLength;
+        int alphaLength;
 
-    pb::File* file = pb::FileSystem::Instance()->OpenFile(location, path);
-    if (!file)
-        return false;
-    
-    file->ReadAll(data);
-    
-    int width, height, components;
-    decoded = stbi_load_from_memory(&data[0], data.size(), &width, &height, &components, STBI_default);
-	
-    if (!glm::isPowerOfTwo(width) || !glm::isPowerOfTwo(height))
-        createMips = false;
-    
-    delete file;
-    
-    bool status = LoadFromBytes(decoded, width, height, createMips, components == 3 ? kTextureFormatRGB : kTextureFormatRGBA, hasPremultipliedAlpha);
-    
-    stbi_image_free(decoded);
+        file->Read(rgbLength);
+        file->Read(alphaLength);
+        
+        unsigned char* encodedRgbData = new unsigned char[rgbLength];
+        unsigned char* encodedAlphaData = new unsigned char[alphaLength];
+        
+        file->Read(encodedRgbData, rgbLength);
+        file->Read(encodedAlphaData, alphaLength);
+        
+        int width, height, components;
+        unsigned char* decodedRgb = stbi_load_from_memory(encodedRgbData, rgbLength, &width, &height, &components, STBI_default);
+        unsigned char* decodedAlpha = stbi_load_from_memory(encodedAlphaData, alphaLength, &width, &height, &components, STBI_default);
+        
+        unsigned char* decoded = new unsigned char[width*height*4];
+        
+        unsigned char* decodedTemp = decoded;
+        unsigned char* rgbTemp = decodedRgb;
+        unsigned char* alphaTemp = decodedAlpha;
+        
+        for (int y=0; y<height; y++)
+        {
+            for (int x=0; x<width; x++)
+            {
+                decodedTemp[0] = rgbTemp[0];
+                decodedTemp[1] = rgbTemp[1];
+                decodedTemp[2] = rgbTemp[2];
+                decodedTemp[3] = *alphaTemp;
+                
+                decodedTemp += 4;
+                rgbTemp += 3;
+                alphaTemp++;
+            }
+        }
+        
+        stbi_image_free(decodedRgb);
+        stbi_image_free(decodedAlpha);
+        
+        status = LoadFromBytes(decoded, width, height, createMips, kTextureFormatRGBA, hasPremultipliedAlpha);
+        
+        delete[] decoded;
+    } else {
+        std::vector<unsigned char> data;
+        unsigned char* decoded;
+
+        pb::File* file = pb::FileSystem::Instance()->OpenFile(location, path);
+        if (!file)
+            return false;
+        
+        file->ReadAll(data);
+        
+        int width, height, components;
+        decoded = stbi_load_from_memory(&data[0], data.size(), &width, &height, &components, STBI_default);
+        
+        if (!glm::isPowerOfTwo(width) || !glm::isPowerOfTwo(height))
+            createMips = false;
+        
+        delete file;
+        
+        status = LoadFromBytes(decoded, width, height, createMips, components == 3 ? kTextureFormatRGB : kTextureFormatRGBA, hasPremultipliedAlpha);
+        
+        stbi_image_free(decoded);
+    }
     
     return status;
 }
