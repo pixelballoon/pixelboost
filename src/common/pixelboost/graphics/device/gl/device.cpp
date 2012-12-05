992ebad46ee6bfdec269435e300513cfe22e1da7
Jamie Hales <jamie@pixelballoon.com>
2012-12-05 22:56:24 +0000

    Flush the state buffer before a glClear

--- src/common/pixelboost/graphics/device/gl/device.cpp
+++ src/common/pixelboost/graphics/device/gl/device.cpp
@@ -214,6 +214,7 @@ unsigned char* GraphicsDeviceGL::CaptureRenderBuffer()
 
 void GraphicsDeviceGL::ClearBuffers()
 {
+    _CurrentState.UpdateAll(_DesiredState);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 }
 
