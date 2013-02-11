33f136954db0352ac5237e060b21e7e6a7c65af8
Jamie Hales <jamie@pixelballoon.com>
2013-02-11 16:13:53 +0000

    Add common database type declaration

---
+++ src/common/pixelboost/db/types/common.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct Vector2Definition
+{
+    float X;
+    float Y;
+};
+
+struct Vector3Definition
+{
+    float X;
+    float Y;
+    float Z;
+};
+
+struct ColorDefinition
+{
+    float R;
+    float G;
+    float B;
+    float A;
+};
