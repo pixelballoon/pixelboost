f626cba654512c1e172410c0b3202fb4fcb7b505
Jamie Hales <jamie@pixelballoon.com>
2013-03-09 17:30:01 +0000

    Optimise matrix creation

--- src/shared/pixelboost/maths/matrixHelpers.h
+++ src/shared/pixelboost/maths/matrixHelpers.h
@@ -21,9 +21,18 @@ inline glm::mat4x4 CreateRotateMatrix(RotationOrder rotationOrder, glm::vec3 rot
 	switch (rotationOrder)
 	{
 		case kRotationOrder_XYZ:
-			transform = glm::rotate(transform, rotation.x, glm::vec3(1,0,0));
-			transform = glm::rotate(transform, rotation.y, glm::vec3(0,1,0));
-			transform = glm::rotate(transform, rotation.z, glm::vec3(0,0,1));
+            if (rotation.x != 0)
+            {
+                transform = glm::rotate(transform, rotation.x, glm::vec3(1,0,0));
+            }
+            if (rotation.y != 0)
+            {
+                transform = glm::rotate(transform, rotation.y, glm::vec3(0,1,0));
+            }
+            if (rotation.z != 0)
+            {
+                transform = glm::rotate(transform, rotation.z, glm::vec3(0,0,1));
+            }
 		break;
 	}
 	
@@ -32,13 +41,16 @@ inline glm::mat4x4 CreateRotateMatrix(RotationOrder rotationOrder, glm::vec3 rot
 
 inline glm::mat4x4 CreateScaleMatrix(glm::vec3 scale, glm::mat4x4 transform = glm::mat4x4())
 {
+    if (scale == glm::vec3(1,1,1))
+    {
+        return transform;
+    }
+    
     return glm::scale(transform, scale);
 }
 
-inline glm::mat4x4 CreateTransformMatrix(RotationOrder rotationOrder, glm::vec3 translate, glm::vec3 rotate, glm::vec3 scale)
+inline glm::mat4x4 CreateTransformMatrix(RotationOrder rotationOrder, glm::vec3 translate, glm::vec3 rotate, glm::vec3 scale, glm::mat4x4 transform = glm::mat4x4())
 {
-	glm::mat4x4 transform;
-
 	transform = CreateTranslateMatrix(translate, transform);
 	transform = CreateRotateMatrix(rotationOrder, rotate, transform);
 	transform = CreateScaleMatrix(scale, transform);
